4e2ae48f074fff3e387018201bc6f0c49f0f597c
Joseph Kamau <mbuguajoseph376@gmail.com>
2026-02-08 18:50:19 +0300

    Fixed icon file types to jpeg

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,14 +11,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/icon-192.png',
+        src: '/icon-192.jpeg',  // Change .png to .jpeg
         sizes: '192x192',
-        type: 'image/png',
+        type: 'image/jpeg',     // Change png to jpeg
       },
       {
-        src: '/icon-512.png',
+        src: '/icon-512.jpeg',  // Change .png to .jpeg
         sizes: '512x512',
-        type: 'image/png',
+        type: 'image/jpeg',     // Change png to jpeg
       },
     ],
   }
